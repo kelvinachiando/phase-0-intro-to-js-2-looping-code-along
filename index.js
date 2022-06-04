d11f616ea6b5e00722806d6c1d8a97f547a2a2b8
kelvinachiando <kelvinostin22gmail.com>
2022-06-05 00:43:15 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards( namesArray, event) {
+  let thankYouCards =[]
+  for (let i =0; i <namesArray.length; i++) {
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return thankYouCards
+}
+function countDown( countDown){
+  while (countDown>0){
+    console.log (countDown);
+  countDown --;
+}
+console.log(countDown)
+}
